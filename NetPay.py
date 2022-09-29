79a620d5a800b99c00198272a2dc399cf88d9bc4
liam13002 <liam_mccolgan1@baylor.edu>
2022-09-28 19:49:07 -0500

    added for loop

--- NetPay.py
+++ NetPay.py
@@ -11,14 +11,28 @@ def Main():
     ded3= pc.PayrollDeduction('food court', '8/17/2022', 15.25, 21547)
     ded4= pc.PayrollDeduction('vending machine', '8/22/2022', 3.00, 58475)
     ded5= pc.PayrollDeduction('vending machine', '8/5/2022', 2.75, 58475)
+
+    deductions = [ded1, ded2, ded3, ded4, ded5]
+
+    net_pay = emp.getSalary()
+
+    
+    for ded in deductions:
+        if pc.PayrollDeduction.getEmployeeID(ded) == emp.getID():
+            net_pay -= pc.PayrollDeduction.getCharge(ded)
+        
+
+    
+
  
    
     print("*** Employee Pay ***")
     print("Name:", ec.Employee.getName(emp))
     print("ID Number:", ec.Employee.getID(emp))
     print("Department:", ec.Employee.getDept(emp))
-    print("Gross Pay: $", float(ec.Employee.getSalary(emp)), sep="")
-    print("Net Pay: $", ec.Employee.getSalary(emp)-ded2.getCharge()-ded4.getCharge()-ded5.getCharge(), sep="")
+    print("Gross Pay: $", ec.Employee.getSalary(emp), sep="")
+    print("Net Pay: $", net_pay)
+
  
  
  
